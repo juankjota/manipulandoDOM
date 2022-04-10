cfe4c73919adf32f304929fe23a5c3b927c6b4e4
juankjota <juankjota@users.noreply.github.com>
2022-04-10 15:04:41 +0000

    deploy: 64cb3e08564603ec6383c84c7cdbcd561bed0904

---
+++ _dist_/index.js
@@ -0,0 +1,6 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+
+const url = https:"//platzi-avo.vercel.app/api/avo";
