addd4c2673a6d3c7236be4e761b3d1a187db7759
juankjota <juankjota@users.noreply.github.com>
2022-04-10 17:38:26 +0000

    deploy: 37ad928ff697a8ba93ee92096a30f6b3aade1298

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,52 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = https:"//platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price)=>{
+    const newPrice = new window.Intl.NumberFormat('en-En',{
+        style:"currency",
+        currency:"GBP",
+    }).format(price)
+    return newPrice;
+}
+
+//web api fetch
+
+//conectarnos al server
+window
+.fetch(`${baseUrl}/api/avo`)
+//procesar la respuesta y convertirla en JSON
+.then(response => response.json())
+//JSON -> Data -> Renderizar info browser
+.then(respuestaJSON =>{
+    const todosLosItems = [];
+    respuestaJSON.data.forEach(element => {
+        //crear imagen
+        const imagen = document.createElement('img');                
+        imagen.src = `${baseUrl}${element.image}`
+        imagen.className = 'imagen';
+        //crear titulo
+        const title = document.createElement('h2');       
+        title.textContent = element.name;
+        title.className = 'text-xl'
+        //crear precio
+        const price = document.createElement('div');        
+        price.textContent = formatPrice(element.price);
+        price.className = 'flex-row tex-gray-600'; 
+
+
+        const container = document.createElement('div');
+        container.append(imagen,title,price);
+        container.className = 'container'
+        
+        todosLosItems.push(container);
+    });
+
+    appNode.append(...todosLosItems);
+})
+
+
+
